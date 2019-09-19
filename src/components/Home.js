dcf633a03d87dd4f7534e3b8df8d00842d74ce72
Blake Clark <blake.17clark@gmail.com>
2019-09-19 15:30:19 -0600

    logo

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,10 +7,11 @@ class Home extends React.Component{
 render(){
   return (
     <div className="App" id="font">
-
-          Welcome To Flowing Freeride
+          <br></br>
+          <h1>Welcome To Flowing Freeride</h1>
+          <img src={require("./ffrvinylstickers-04.jpg")} alt="ffr" style={{ height: "430px" }}/>
           </div>
-
+          
           
           );
 }
