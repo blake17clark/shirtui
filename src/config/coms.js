470a9b6bfd654bbb0ed1bfb5c091c7c00073638d
Blake Clark <blake.17clark@gmail.com>
2019-10-15 13:09:41 -0600

    color css

--- src/config/coms.js
+++ src/config/coms.js
@@ -1,2 +1,2 @@
-export const SHIRT_API = process.env.REACT_APP_SHIRT_API || "https://shirtapi2.herokuapp.com"
-// export const SHIRT_API = "http://localhost:4000"
+// export const SHIRT_API = process.env.REACT_APP_SHIRT_API || "https://shirtapi2.herokuapp.com"
+export const SHIRT_API = "http://localhost:4000"
